4ccef732d2affb218a459d417573bb0b31a74b20
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-17 15:48:23 +0100

    Add `Save Movie` functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -4,6 +4,9 @@ import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  
+  console.log(props);
+  
  
   useEffect(() => {
     const id = props.match.params.id;
@@ -23,10 +26,10 @@ const Movie = (props) => {
   },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.save;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -52,7 +55,7 @@ const Movie = (props) => {
         ))}
       </div> */}
       <MovieCard movie={movie} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 }
